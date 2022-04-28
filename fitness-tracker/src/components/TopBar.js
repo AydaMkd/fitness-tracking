9c864de8998eaa990b7c9e63247f38d5969e5378
Christina <christina.hurt@purplewave.com>
2022-04-27 21:37:01 -0500

    set up NavBar

---
+++ fitness-tracker/src/components/TopBar.js
@@ -0,0 +1,26 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+
+function TopBar () {
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
+              <Container>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="me-auto">
+                    <Nav.Link href="#home">Fitness Tracker</Nav.Link>
+                    <Nav.Link href="#link">Activity</Nav.Link>
+                    <Button variant="outline-secondary">Log In</Button>
+                    <Nav.Link href="#link">Create Account</Nav.Link>
+                  </Nav>
+                </Navbar.Collapse>
+              </Container>
+            </Navbar>
+        </div>
+    );
+}
+
+export default TopBar;
