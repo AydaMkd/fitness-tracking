00b0384e6a3b48926edb83da7a49ab1bdff90f4f
MyMac <mymac@MyMacs-MBP.lan>
2022-04-28 17:47:10 -0500

    commit

--- fitness-tracker/src/components/Activity.js
+++ fitness-tracker/src/components/Activity.js
@@ -6,9 +6,9 @@ function Activity() {
         <label>Username
           <input type="text" />
         </label><br/>
-        <label for="exercice">Choose an exercice</label>
-        <select id="exercice">
-                <option value="running">running</option>
+        <label for="exercise">Choose an exercice</label>
+        <select id="exercise">
+                <option value="running">Running</option>
                 <option value="walkin">Walking</option>
                 <option value="swimming">Swimming</option>
                 <option value="cycling">Cycling</option>
@@ -19,7 +19,7 @@ function Activity() {
       </label><br/>
       <label for="date">Date:</label>
 
-     <input type="date" id="date" name="exercice date"
+     <input type="date" id="date" name="exercise date"
        value="2022-04-28"
        min="2022-01-01"/><br/>
       <input type="submit" value="Submit"></input>  
